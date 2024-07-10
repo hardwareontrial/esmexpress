d8d3fb249b1d4e6ea72766cde9b30ffad0b619db
it <hadwareontrial@gmail.com>
2024-07-10 21:50:14 +0700

    change table_name caused different name on db

--- models/DatabaseA/hr.mjs
+++ models/DatabaseA/hr.mjs
@@ -58,45 +58,6 @@ AppHrPositionUser.init({
   underscored: false,
   schema: db.DatabaseA.config.database,
 });
-// AppHrDepartment.init({
-//   id: {type: DataTypes.BIGINT(20).UNSIGNED, allowNull: false, primaryKey: true, autoIncrement: true},
-//   dept_unique_str: { type: DataTypes.STRING, allowNull: false, unique: true },
-//   name: {type:DataTypes.STRING(255), allowNull: false},
-//   parent_id: {type:DataTypes.BIGINT(20).UNSIGNED, allowNull: true},
-//   is_active: {type:DataTypes.TINYINT(1), allowNull: false, defaultValue: 1},
-// },{
-//   sequelize: db.DatabaseA,
-//   modelName: 'AppHrDepartment',
-//   freezeTableName: true,
-//   tableName: 'tbl_hr_departments',
-//   timestamps: false,
-//   createdAt: 'created_at',
-//   updatedAt: 'updated_at',
-//   underscored: false,
-//   schema: db.DatabaseA.config.database,
-// });
-
-// AppHrPosition.init({
-//   id: {type: DataTypes.BIGINT(20).UNSIGNED, allowNull: false, noUpdate: false, primaryKey: true, autoIncrement: true},
-//   position_unique_str: { type: DataTypes.STRING, allowNull: false, unique: true },
-//   parent_id: {type:DataTypes.BIGINT(20).UNSIGNED, allowNull: true, noUpdate: false},
-//   name: {type:DataTypes.STRING(255), allowNull: false, noUpdate: false},
-//   dept_id: {type:DataTypes.BIGINT(20).UNSIGNED, allowNull: true, noUpdate: false},
-//   sub_dept_id: {type:DataTypes.BIGINT(20).UNSIGNED, allowNull: true, noUpdate: false},
-//   level: {type:DataTypes.INTEGER(11), allowNull: true, noUpdate: false},
-//   user_id: {type:DataTypes.BIGINT(20).UNSIGNED, allowNull: true, noUpdate: false},
-//   is_active: {type:DataTypes.TINYINT(1), allowNull: false, noUpdate: false, defaultValue: 1},
-// },{
-//   sequelize: db.DatabaseA,
-//   modelName: 'AppHrPosition',
-//   freezeTableName: true,
-//   tableName: 'tbl_hr_positions',
-//   timestamps: false,
-//   createdAt: 'created_at',
-//   updatedAt: 'updated_at',
-//   underscored: false,
-//   schema: db.DatabaseA.config.database,
-// });
 
 TmpAttendance.init({
   scan_date: {type: DataTypes.DATE(), allowNull: false},
@@ -108,7 +69,7 @@ TmpAttendance.init({
   sequelize: db.DatabaseA,
   modelName: 'TmpAttendance',
   freezeTableName: true,
-  tableName: 'tbl_tmpAttn',
+  tableName: 'tbl_tmpattn',
   timestamps: true,
   createdAt: 'created_at',
   updatedAt: false,
@@ -125,7 +86,7 @@ TmpAttnLog.init({
   sequelize: db.DatabaseA,
   modelName: 'TmpAttnLog',
   freezeTableName: true,
-  tableName: 'tbl_tmpAttn_log',
+  tableName: 'tbl_tmpattn_log',
   timestamps: true,
   createdAt: 'created_at',
   updatedAt: false,
