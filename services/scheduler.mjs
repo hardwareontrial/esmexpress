ba36bd9aa5f829c261080ffa39b277f539297d0d
hardwareontrial <hadwareontrial@gmail.com>
2024-08-07 11:53:49 +0700

    simplified code

--- services/scheduler.mjs
+++ services/scheduler.mjs
@@ -1,25 +1,16 @@
 import nodeCron from 'node-cron'
 import moment from 'moment'
 import QueueService from '@services/queue.mjs'
+import pc from "picocolors";
 
 const init = () => {
-  /** REFRESH AND SORTED QUEUE (EVERY 4min31sec) */
-  nodeCron.schedule('31 */4 * * * *', async () => {
-    try {
-      const getAllQueue = await QueueService.getQueueAll()
-      console.log('Queue updated')
-    } catch (error) {
-      console.error(error)
-    }
-  });
 
-  /** EXECUTE QUEUE (EVERY 12sec) */
-  nodeCron.schedule('*/12 * * * * *', async () => {
+  /** EXECUTE QUEUE (EVERY 30s) */
+  nodeCron.schedule('*/30 * * * * *', async () => {
     try {
-      const inFront = await QueueService.frontQueue()
-      if(inFront) { await QueueService.processingQueue(inFront) }
+      await QueueService.initQueue();
     } catch (error) {
-      console.error(error)      
+      console.log(pc.bgRed(pc.white(`SCHEDULE-QUEUE: ${error}`)));
     }
   });
 }
