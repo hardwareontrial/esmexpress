af83af5c368a291ae33b1c6b9cd0e1d37e2ba931
hardwareontrial <hadwareontrial@gmail.com>
2024-08-13 21:19:01 +0700

    completing validator for material, material-content, question-collection, question-content

--- middlewares/validators/okm.mjs
+++ middlewares/validators/okm.mjs
@@ -1,25 +1,93 @@
-import OKMServices from '@services/apps/okm.mjs'
+import OKMServices from '@services/apps/okm.mjs';
+import pc from 'picocolors';
 
-const inputMaterialValidator = async (req,res,next) => {
-  const isValid = await OKMServices.validatorInputMaterial({
-    file: req.file,
-    body: req.body,
-  });
-  if(!isValid.success) {
-    return res.status(422).send({success: false, message: 'Unprocessable Entity', data: isValid.message})
-  } else {
-    next()
+const inputMaterialValidator = async (req, res, next) => {
+  try {
+    const isMaterialValid = OKMServices.inputMaterialValidator(req.body.title, req.body.sinopsis, req.body.level, req.body.deptId, req.body.isActive, req.body.creator);
+
+    const isMaterialContentValid = OKMServices.inputMaterialContentValidator(req.body.materialId, req.body.description, req.body.filepath, req.body.viewCount);
+
+    let isMaterialFileValid = { success: true, message: [] }
+    if(req.file) {
+      const validating = OKMServices.inputMaterialFileValidator(req.file);
+      isMaterialFileValid = { success: validating.success, message: validating.message }
+    }
+
+    const result = {
+      success: isMaterialValid.message.length === 0 && isMaterialContentValid.message.length === 0 && isMaterialFileValid.message.length === 0,
+      message: [...isMaterialValid.message, ...isMaterialContentValid.message, ...isMaterialFileValid.message]
+    }
+
+    if(!result.success) { return res.status(422).send({success: false, message: 'Unprocessable Entity', data: result.message}) }
+    return next()
+  } catch (error) {
+    console.log(pc.bgRed(pc.white(`VALIDATOR-inputMaterialValidator: ${error.message || error}`)));
+    return res.status(500).send({ success: false, message: 'Internal Server Error', data: [] });
   }
 };
 
-const inputMaterialContentValidator = async (req,res,next) => {
-  // only validating file
-  const isValid = await OKMServices.validatorFileMaterial(req.file)
-  if(!isValid.success) {
-    return res.status(422).send({success: false, message: 'Unprocessable Entity', data: isValid.message})
-  } else {
-    next()
-  }
-}
+const inputMaterialContentValidator = (req, res, next) => {
+  try {
+    const isMaterialContentValid = OKMServices.inputMaterialContentValidator(req.body.materialId, req.body.description, req.body.filepath, req.body.viewCount);
+    
+    let isMaterialFileValid = { success: true, message: [] }
+    if(req.file) {
+      const validating = OKMServices.inputMaterialFileValidator(req.file);
+      isMaterialFileValid = { success: validating.success, message: validating.message }
+    }
+    
+    const result = {
+      success: isMaterialContentValid.message.length === 0 && isMaterialFileValid.message.length === 0,
+      message: [...isMaterialContentValid.message, ...isMaterialFileValid.message]
+    }
 
-export { inputMaterialValidator, inputMaterialContentValidator }
+    if(!result.success) { return res.status(422).send({success: false, message: 'Unprocessable Entity', data: result.message}) }
+    return next();
+  } catch (error) {
+    console.log(pc.bgRed(pc.white(`VALIDATOR-inputMaterialContentValidator: ${error.message || error}`)));
+    return res.status(500).send({ success: false, message: 'Internal Server Error', data: [] });
+  }
+};
+
+const inputQuestionCollectionValidator = async (req, res, next) => {
+  try {
+    const isQuestionCollValid = OKMServices.inputQuestionCollectionValidator(req.body.materialContentId, req.body.title, req.body.level, req.body.isActive, req.body.creator);
+    
+    let isQuestionFileValid = { success: true, message: [] }
+    if(req.file) {
+      const validating = OKMServices.inputQuestionFileValidator(req.file);
+      isQuestionFileValid = { success: validating.success, message: validating.message }
+    }
+
+    const result = {
+      success: isQuestionCollValid.message.length === 0 && isQuestionFileValid.message.length === 0,
+      message: [...isQuestionCollValid.message, ...isQuestionFileValid.message]
+    }
+    
+    if(!result.success) { return res.status(422).send({success: false, message: 'Unprocessable Entity', data: result.message}) }
+    return next();
+  } catch (error) {
+    console.log(pc.bgRed(pc.white(`VALIDATOR-inputQuestionCollectionValidator: ${error.message || error}`)));
+    return res.status(500).send({ success: false, message: 'Internal Server Error', data: [] });
+  }
+};
+
+const inputQuestionContentValidator = async (req, res,next) => {
+  try {
+    //
+  } catch (error) {
+    console.log(pc.bgRed(pc.white(`VALIDATOR-inputQuestionContentValidator: ${error.message || error}`)));
+    return res.status(500).send({ success: false, message: 'Internal Server Error', data: [] });
+  }
+};
+
+const inputQuizValidator = async  (req, res, next) => {
+  try {
+    //
+  } catch (error) {
+    console.log(pc.bgRed(pc.white(`VALIDATOR-inputQuizValidator: ${error.message || error}`)));
+    return res.status(500).send({ success: false, message: 'Internal Server Error', data: [] });
+  }
+};
+
+export { inputMaterialValidator, inputMaterialContentValidator, inputQuestionCollectionValidator }
