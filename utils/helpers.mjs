4f244fd9bf2968737ce626e782c77bba1f06b7f8
hardwareontrial <hadwareontrial@gmail.com>
2024-09-10 10:47:29 +0700

    add new functions

--- utils/helpers.mjs
+++ utils/helpers.mjs
@@ -36,3 +36,25 @@ export async function getNextAutoIncrementValue(tableName) {
   const [result] = await db.DatabaseA.query(`SHOW TABLE STATUS LIKE '${tableName}'`);
   return result[0].Auto_increment;
 }
+
+export function acceptingMime() {
+  return ['application/pdf', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'image/jpeg', 'image/jpg'];
+}
+
+export function acceptingSize(mimetype) {
+  if(mimetype === 'application/pdf') {}
+  else if(mimetype === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet') {
+    return 6 *1024*1024
+  }
+  else if(mimetype === 'image/jpeg' || mimetype === 'image/jpg') {
+    return 1 *1024*1024
+  }
+  else {
+    return 0 *1024*1024
+  }
+}
+
+export function normalizedArray(data) {
+  if(Array.isArray(data) && data.length === 1 && data[0] === '') { return [] }
+  return data;
+}
