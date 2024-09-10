3d4d11e5cc273b8abce22b76d268c704f72f0787
hardwareontrial <hadwareontrial@gmail.com>
2024-09-10 10:43:09 +0700

    major changes

--- routes/DatabaseA/okm.mjs
+++ routes/DatabaseA/okm.mjs
@@ -1,59 +1,44 @@
-import { Router } from 'express'
-import checkToken from '@middlewares/routes/token.mjs'
-import checkRoutePermission from '@middlewares/routes/permissions.mjs'
-import {
-  getAllMaterial, getMaterialPaginate, createMaterial, detailMaterial, updateMaterial, 
-  getAllMaterialContent, getMaterialContentByMaterialId, detailMaterialContent, updateMaterialContent,
-  getAllQuestionsCollection, getQuestionCollectionPaginate, createQuestionCollection, detailQuestionCollection, updateQuestionCollection,
-  getAllQuestionContent, getQuestionContentByCollectiondId, detailQuestionContent,
-  testing,
-} from '@controllers/DatabaseA/okm.mjs'
-import multer from 'multer'
+import { Router } from 'express';
+import checkToken from '@middlewares/routes/token.mjs';
+import checkRoutePermission from '@middlewares/routes/permissions.mjs';
+import multer from 'multer';
 
-import { inputMaterialValidator, inputMaterialContentValidator, inputQuestionCollectionValidator } from '@middlewares/validators/okm.mjs'
+import {
+  getAllMaterials, detailMaterial, createMaterial, updateMaterial,
+  getAllMaterialContents, detailMaterialContent, createMaterialContent, updateMaterialContent,
+  getAllQuestionCollection, detailQuestionCollection, createQuestionCollection, updateQuestionCollection,
+  getAllQuestionContent, detailQuestionContent, createQuestionContent, updateQuestionContent,
+  getAllQuestionOpt, detailQuestionOpt, createQuestionOpt, updateQuestionOpt,
+  getParticipantList,
+} from '@controllers/DatabaseA/okm.mjs';
 
 const route = Router();
 
-route.get('/materials', checkToken, checkRoutePermission(['manage.all']), getAllMaterial);
-route.get('/material-paginate', checkToken, checkRoutePermission(['manage.all']), getMaterialPaginate);
-route.post('/material', checkToken, checkRoutePermission(['manage.all']),
-  multer({storage: multer.memoryStorage()}).single('file'),
-  inputMaterialValidator,
-  createMaterial
-);
+route.get('/materials', checkToken, checkRoutePermission(['manage.all']), getAllMaterials);
+route.post('/material', checkToken, checkRoutePermission(['manage.all']), multer({storage: multer.memoryStorage()}).any(), createMaterial);
 route.get('/material/:id', checkToken, checkRoutePermission(['manage.all']), detailMaterial);
-route.put('/material/:id', checkToken, checkRoutePermission(['manage.all']), inputMaterialValidator, updateMaterial);
+route.put('/material/:id', checkToken, checkRoutePermission(['manage.all']), updateMaterial);
 
-route.get('/material-content', checkToken, checkRoutePermission(['manage.all']), getAllMaterialContent);
-route.get('/material-content/:materialId', checkToken, checkRoutePermission(['manage.all']), getMaterialContentByMaterialId);
-route.get('/material-content/:contentId', checkToken, checkRoutePermission(['manage.all']), detailMaterialContent);
-route.post('/material-content/:contentId',
-  checkToken,
-  checkRoutePermission(['manage.all']),
-  multer({storage: multer.memoryStorage()}).single('file'),
-  inputMaterialContentValidator,
-  updateMaterialContent
-);
+route.get('/material-contents', checkToken, checkRoutePermission(['manage.all']), getAllMaterialContents);
+route.post('/material-content', checkToken, checkRoutePermission(['manage.all']), multer({storage: multer.memoryStorage()}).single('material'), createMaterialContent);
+route.get('/material-content/:id', checkToken, checkRoutePermission(['manage.all']), detailMaterialContent);
+route.post('/material-content/:id', checkToken, checkRoutePermission(['manage.all']), multer({storage: multer.memoryStorage()}).single('material'), updateMaterialContent);
 
-route.get('/question-collection', checkToken, checkRoutePermission(['manage.all']), getAllQuestionsCollection);
-route.get('/question-collection-paginate', checkToken, checkRoutePermission(['manage.all']), getQuestionCollectionPaginate);
+route.get('/question-collections', checkToken, checkRoutePermission(['manage.all']), getAllQuestionCollection);
+route.post('/question-collection', checkToken, checkRoutePermission(['manage.all']), multer({storage: multer.memoryStorage()}).any(), createQuestionCollection);
 route.get('/question-collection/:id', checkToken, checkRoutePermission(['manage.all']), detailQuestionCollection);
-route.post('/question-collection',
-  checkToken,
-  checkRoutePermission(['manage.all']),
-  multer({storage: multer.memoryStorage()}).single('fileExcel'),
-  inputQuestionCollectionValidator,
-  createQuestionCollection
-);
-route.put('/question-collection/:id', checkToken, checkRoutePermission(['manage.all']), inputQuestionCollectionValidator, updateQuestionCollection);
+route.put('/question-collection/:id', checkToken, checkRoutePermission(['manage.all']), updateQuestionCollection);
 
-route.get('/question-content', checkToken, checkRoutePermission(['manage.all']), getAllQuestionContent);
-route.get('/question-content/:collectionId/collection', checkToken, checkRoutePermission(['manage.all']), getQuestionContentByCollectiondId);
-route.get('/question-content/:id/detail', checkToken, checkRoutePermission(['manage.all']), detailQuestionContent);
-route.post('/question-content', checkToken, checkRoutePermission(['manage.all']));
-route.post('/question-content/:id', checkToken, checkRoutePermission(['manage.all']));
+route.get('/question-contents', checkToken, checkRoutePermission(['manage.all']), getAllQuestionContent);
+route.post('/question-content', checkToken, checkRoutePermission(['manage.all']), multer({storage: multer.memoryStorage()}).any(), createQuestionContent);
+route.get('/question-content/:id', checkToken, checkRoutePermission(['manage.all']), detailQuestionContent);
+route.post('/question-content/:id', checkToken, checkRoutePermission(['manage.all']), multer({storage: multer.memoryStorage()}).single('question_media'), updateQuestionContent);
 
-// route.post('/testing', checkToken, checkRoutePermission(['manage.all']), testing);
-route.post('/testing', checkToken, checkRoutePermission(['manage.all']), multer({storage: multer.memoryStorage()}).any(), testing);
+route.get('/question-options', checkToken, checkRoutePermission(['manage.all']), getAllQuestionOpt);
+route.post('/question-option', checkToken, checkRoutePermission(['manage.all']), multer({storage: multer.memoryStorage()}).single('question_option_media'), createQuestionOpt);
+route.get('/question-option/:id', checkToken, checkRoutePermission(['manage.all']), detailQuestionOpt);
+route.post('/question-option/:id', checkToken, checkRoutePermission(['manage.all']), multer({storage: multer.memoryStorage()}).single('question_option_media'), updateQuestionOpt);
+
+route.get('/list/participants', checkToken, checkRoutePermission(['manage.all']), getParticipantList);
 
 export default route;
