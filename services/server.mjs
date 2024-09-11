dae63251fe978b490b00b1eab6c49339858db9c8
hardwareontrial <hadwareontrial@gmail.com>
2024-09-11 14:33:55 +0700

    change server to provide https

--- services/server.mjs
+++ services/server.mjs
@@ -1,6 +1,18 @@
 import http from 'http'
+import https from 'https';
+import fs from 'fs';
 import app from '@services/express.mjs'
+import path from 'path';
 
-const server = http.createServer(app)
+let privateKey = fs.readFileSync(path.join(path.resolve(), '/public/certificates/development.key'), 'utf8');
+let certificate = fs.readFileSync(path.join(path.resolve(), '/public/certificates/development.crt'), 'utf8');
+if(process.env.NODE_ENV === 'production') {
+  privateKey = fs.readFileSync(path.join(path.resolve(), '/public/certificates/server.key'), 'utf8');
+  certificate = fs.readFileSync(path.join(path.resolve(), '/public/certificates/server.crt'), 'utf8');
+}
+const credentials = { key: privateKey, cert: certificate }
+
+// const server = http.createServer(app)
+const server = https.createServer(credentials, app);
 
 export default server
